01991c6637a09108ac8a6257e9575265935e7cde
un <mapzine123@naver.com>
2023-08-29 00:25:47 +0900

    #2 - App.tsx 파일 수정
    Route의 Path값으로 설정된 것처럼 /admins/의 url을 가지는 도메인은 모두 AdminApp에서 관리
    
    후에 다른 route를 `/members/*` 로 설정해서 Admin과 Member 페이지를 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Routes로 변경
+import AdminApp from './pages/admins/AdminApp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/admins/*" element={<AdminApp />} />
+      </Routes>
+    </Router>
   );
 }
 
