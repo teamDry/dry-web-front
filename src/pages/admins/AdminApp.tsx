bff93f0d8283063652ada48e7b9739ed62caae62
un <mapzine123@naver.com>
2023-08-30 00:58:26 +0900

    #4 - AdminApp에 LoginPage 매핑
    애초에 관리자 페이지는 만드시 로그인 이후에 사용할 수 있게끔 하고싶어서 루트페이지를 로그인 페이지로 변경

--- src/pages/admins/AdminApp.tsx
+++ src/pages/admins/AdminApp.tsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Routes로 변경
 import MainPage from './MainPage';
-
+import LoginPage from './LoginPage';
+import SignUpPage from './SignUpPage';
 
 // Admin 페이지 쪽 App.tsx
 const AdminApp = () => {
 
-    return (
-        <Routes>
-          <Route path="main" element={<MainPage />} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="main" element={<MainPage />} />
+      <Route path="/" element={<LoginPage />} />
+    </Routes>
+  )
 
 }
 
