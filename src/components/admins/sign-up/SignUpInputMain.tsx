cc2cbbdd0bf39a18ec65bd996945257999219b5a
un <mapzine123@naver.com>
2023-09-03 02:25:57 +0900

    #10 - SignUp View 수정 및 css 파일 작성
    SignUpLogic의 함수와 컴포넌트 이벤트 매핑
    입력 작성이 잘 됐는지에 대한 신호 표시용 css파일

--- src/components/admins/sign-up/SignUpInputMain.tsx
+++ src/components/admins/sign-up/SignUpInputMain.tsx
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
 import styled from 'styled-components'
 import { prefixAdminImagePath } from '../../../utils/admins/AdminUtil';
 import SignUpInputBox from './SignUpInputBox';
+import { idCheck, nicknameCheck, passwordCheck, passwordRecheck } from './static/SignUpLogic';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../reducers/RootReducer';
 
 const SignUpInputMainDiv = styled.main`
     width: 60%;
@@ -16,34 +18,48 @@ const SignUpInputDiv = styled.div`
 `
 
 const SignUpInputMain = () => {
+
     const inputData = [{
         type: "text",
         id: "id",
+        name: "id",
         img: prefixAdminImagePath("login-user.svg"),
         placeholder: "ID",
+        onBlur: idCheck,
+        checkData: useSelector((state: RootState) => state.adminSignUp.idCheck)
+
     }, {
         type: "password",
         id: "password",
+        name: "password",
         img: prefixAdminImagePath("login-password.svg"),
         placeholder: "PASSWORD",
+        onBlur: passwordCheck,
+        checkData: useSelector((state: RootState) => state.adminSignUp.passwordCheck)
     }, {
         type: "password",
         id: "password-check",
+        name: "password-check",
         img: prefixAdminImagePath("signUp-password-check.svg"),
         placeholder: "PASSWORD CHECK",
+        onBlur: passwordRecheck,
+        checkData: useSelector((state: RootState) => state.adminSignUp.passwordReCheck)
     }, {
         type: "text",
         id: "nickname",
+        name: "nickname",
         img: prefixAdminImagePath("signUp-nickname.svg"),
         placeholder: "NICKNAME",
+        onBlur: nicknameCheck,
+        checkData: useSelector((state: RootState) => state.adminSignUp.nicknameCheck)
     }]
 
     return (
         <SignUpInputMainDiv>
             <SignUpInputDiv>
-            {inputData.map((data, index) => (
-                <SignUpInputBox key={index} type={data.type} id={data.id} img={data.img} placeholder={data.placeholder} />
-            ))}
+                {inputData.map((item, index) => (
+                    <SignUpInputBox key={index} type={item.type} id={item.id} name={item.name} img={item.img} placeholder={item.placeholder} onBlur={item.onBlur} data={item.checkData} />
+                ))}
             </SignUpInputDiv>
         </SignUpInputMainDiv>
     )
